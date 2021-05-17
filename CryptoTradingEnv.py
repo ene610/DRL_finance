c6e843baa41ecd6d128f7652219d9f667bee7d51
aleph <car.morelli3@gmail.com>
2021-05-17 12:34:24 +0200

    added maxProfit function
    function that return the max profit possible in the env , used for evalueting agents

--- CryptoTradingEnv.py
+++ CryptoTradingEnv.py
@@ -358,3 +358,30 @@ class CryptoTradingEnv(gym.Env):
         filtered_signal_feature = signal_features.drop(columns=indicators_column_to_filter)
 
         return filtered_signal_feature
+
+    def maxProfit(self):
+
+        price = self._prices[self.window_size:]
+        k = len(price) / 2
+        n = len(price)
+
+
+
+        # Bottom-up DP approach
+        profit = [[0 for i in range(k + 1)] for j in range(n)]
+
+        # Profit is zero for the first
+        # day and for zero transactions
+        for i in range(1, n):
+
+            for j in range(1, k + 1):
+                max_so_far = 0
+
+                for l in range(i):
+                    max_so_far = max(max_so_far, price[i] -
+                                     price[l] + profit[l][j - 1])
+
+                profit[i][j] = max(profit[i - 1][j], max_so_far)
+
+        print(profit[n - 1][k])
+
