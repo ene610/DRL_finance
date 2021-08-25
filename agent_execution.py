94f83b30cf67612671f21924d0e073d1809d4208
aleph <car.morelli3@gmail.com>
2021-08-25 17:38:27 +0200

    minor bufixes

--- agent_execution.py
+++ agent_execution.py
@@ -47,8 +47,8 @@ row = {
     }
 
 def load_agent(id_agent, path):
-    agents_csv = "tuning\\agents.csv"
-    df_agent = pd.read_csv(path + "\\" + agents_csv, sep=";")
+    agents_csv = "tuning/agents.csv"
+    df_agent = pd.read_csv(path + "/" + agents_csv, sep=";")
     df_agent = df_agent.set_index("agent_id")
     agents = df_agent.to_dict(orient="index")
     agent = agents[id_agent]
@@ -57,9 +57,9 @@ def load_agent(id_agent, path):
 
 def load_env(id_env, path):
 
-    envs_csv = "tuning\\envs.csv"
+    envs_csv = "tuning/envs.csv"
 
-    df_env = pd.read_csv(path + "\\" + envs_csv, sep=";")
+    df_env = pd.read_csv(path + "/" + envs_csv, sep=";")
     df_env = df_env.set_index("env_id")
     envs = df_env.to_dict(orient="index")
     env = envs[id_env]
@@ -68,27 +68,27 @@ def load_env(id_env, path):
 
 def insert_agent_row(agent_hyperparameter, path):
 
-    agents_csv = "tuning\\agents.csv"
+    agents_csv = "tuning/agents.csv"
     agent_row = pd.DataFrame.from_dict(agent_hyperparameter, orient='index')
     agent_row = agent_row.transpose()
     agent_row = agent_row.set_index("agent_id")
 
-    agent_row.to_csv(path + "\\" + agents_csv, sep=";", mode='a', header=None)
+    agent_row.to_csv(path + "/" + agents_csv, sep=";", mode='a', header=None)
 
 def insert_env_row(env_parameter, path):
-    envs_csv = "tuning\\envs.csv"
+    envs_csv = "tuning/envs.csv"
     env_row = pd.DataFrame.from_dict(env_parameter, orient='index')
     env_row = env_row.transpose()
     env_row = env_row.set_index("env_id")
 
-    env_row.to_csv(path + "\\" + envs_csv, sep=";", mode='a', header=None)
+    env_row.to_csv(path + "/" + envs_csv, sep=";", mode='a', header=None)
 
 def load_data(coin):
     # Load data
     path = os.getcwd()
 
 
-    df = pd.read_csv(f"{path}\\data\\Binance_{coin}USDT_minute.csv", skiprows=1)
+    df = pd.read_csv(f"{path}/data/Binance_{coin}USDT_minute.csv", skiprows=1)
     df = df.rename(columns={'Volume USDT': 'volume'})
     df = df.iloc[::-1]
     df = df.drop(columns=['symbol', f"Volume {coin}"])
@@ -153,6 +153,6 @@ def select_agent(id_agent,env,coin):
 
 coin = "BTC"
 env = select_env(20,coin)
-agent = select_agent(1,env,coin)
+agent = select_agent(1, env, coin)
 
 
